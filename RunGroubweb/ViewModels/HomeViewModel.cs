6f50c4afe66306ce9b3baf01e4ae1355e0e196ed
assem elgokhy <319190407@sha.edu.eg>
2022-12-12 14:42:37 +0200

    final update

---
+++ RunGroubweb/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RunGroubweb.Models;
+
+namespace RunGroubweb.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set;}
+
+        public string City { get; set;}
+
+        public string State { get; set;}
+    }
+}
